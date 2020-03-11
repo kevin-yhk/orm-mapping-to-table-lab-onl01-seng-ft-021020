10b34975334fc06a26d1dfe5800bb511f678d802
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-11 20:25:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,9 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id 
   
-  def initialize(name, grade, id = 
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade
+  end 
   
 end
