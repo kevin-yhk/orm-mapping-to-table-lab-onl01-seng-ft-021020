b3b35417b8a92802578174d1ddf5f3f2b9ed3211
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-11 20:42:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
     
-  def self.create(attributes)
-    student = Student.new(attributes[:name], grade)
+  def self.create(attri)
+    student = Student.new(attri[:name], attri[:grade])
     student.save
     student 
   end 
